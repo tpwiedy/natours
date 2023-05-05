6ffba3caba712fecacff322c7c8f6f3ff1244970
tpwiedy <wiedy.pratama@gmail.com>
2023-05-05 10:37:09 +0700

    adding a reviewModel

---
+++ models/reviewModel.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+const User = require('./userModel');
+const Tour = require('./tourModel');
+
+const reviewSchema = new mongoose.Schema({
+  review: {
+    type: String,
+    required: [true, 'Review can not be empty!'],
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+    // VALIDATOR
+    min: [1, 'Rating must be above 1.0'],
+    max: [5, 'Rating must be below 5.0'],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  tour: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'Tour',
+    required: [true, 'Review must belong to a tour.'],
+  },
+  user: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'User',
+    required: [true, 'Review must belong to a user.'],
+  },
+  // show the virtual properties in results
+  toJSON: { virtuals: true },
+  toObject: { virtuals: true },
+});
+
+const Review = mongoose.model('Review', reviewSchema);
+module.exports = Review;
