9f273a9c15fdb663939b2c98c204f7224795247e
tpwiedy <wiedy.pratama@gmail.com>
2023-05-15 09:21:47 +0700

    Adding a middleware to calculate average and number of rating

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-const User = require('./userModel');
 const Tour = require('./tourModel');
 
 const reviewSchema = new mongoose.Schema(
@@ -37,18 +36,59 @@ const reviewSchema = new mongoose.Schema(
   }
 );
 
+// Compound Index to allowed the one User only review one Tour
+reviewSchema.index({ tour: 1, user: 1 }, { unique: true });
+
 reviewSchema.pre(/^find/, function (next) {
   this.populate({
     path: 'user',
     select: 'name _id',
   });
-
-  // this.populate({
-  //   path: 'user',
-  //   select: 'name _id',
-  // });
-
   next();
+});
+
+reviewSchema.statics.calcAverageRatings = async function (tourId) {
+  const stats = await this.aggregate([
+    {
+      $match: { tour: tourId },
+    },
+    {
+      $group: {
+        _id: '$tour',
+        nRating: { $sum: 1 },
+        avgRating: { $avg: '$rating' },
+      },
+    },
+  ]);
+  // console.log(stats);
+
+  if (stats.length > 0) {
+    await Tour.findByIdAndUpdate(tourId, {
+      ratingsQuantity: stats[0].nRating,
+      ratingsAverage: stats[0].avgRating,
+    });
+  } else {
+    await Tour.findByIdAndUpdate(tourId, {
+      ratingsQuantity: 0,
+      ratingsAverage: 4.5,
+    });
+  }
+};
+
+reviewSchema.post('save', function () {
+  // this point to current review
+  this.constructor.calcAverageRatings(this.tour);
+});
+
+reviewSchema.pre(/^findOneAnd/, async function (next) {
+  this.r = await this.findOne(); //r = review
+  // console.log(this.r);
+  next();
+});
+
+reviewSchema.post(/^findOneAnd/, async function () {
+  // await this.findOne(); does NOT work here, query has already executed
+  await this.r.constructor.calcAverageRatings(this.r.tour);
 });
 
 const Review = mongoose.model('Review', reviewSchema);
