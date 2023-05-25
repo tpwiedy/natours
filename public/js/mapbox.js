2abe5b89adb8ca3b334cbea4f7fd4a23826f27af
tpwiedy <wiedy.pratama@gmail.com>
2023-05-25 10:55:10 +0700

    Add Map view using leaflet.js

--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -1,27 +1,34 @@
 /*eslint-disable*/
-const locations = JSON.parse(document.getElementById('map').dataset.locations);
 
-var map = L.map('map', { zoomControl: false });
+export const displayMap = (locations) => {
+  var map = L.map('map', { zoomControl: false });
+  var markerIcon = L.divIcon({
+    className: 'marker',
+    iconSize: [32, 40],
+    iconAnchor: [22, 94],
+    popupAnchor: [-3, -76],
+  });
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  // maxZoom: 10,
-  attribution:
-    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-  crossOrigin: '',
-}).addTo(map);
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    // maxZoom: 10,
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    crossOrigin: '',
+  }).addTo(map);
 
-const points = [];
-locations.forEach((loc) => {
-  points.push([loc.coordinates[1], loc.coordinates[0]]);
-  L.marker([loc.coordinates[1], loc.coordinates[0]])
-    .addTo(map)
-    .bindPopup(`<p>Day ${loc.day}: ${loc.description}</p>`, {
-      autoClose: false,
-    })
-    .openPopup();
-});
+  const points = [];
+  locations.forEach((loc) => {
+    points.push([loc.coordinates[1], loc.coordinates[0]]);
+    L.marker([loc.coordinates[1], loc.coordinates[0]], { icon: markerIcon })
+      .addTo(map)
+      .bindPopup(`<p>Day ${loc.day}: ${loc.description}</p>`, {
+        autoClose: false,
+      })
+      .openPopup();
+  });
 
-const bounds = L.latLngBounds(points).pad(0.5);
-map.fitBounds(bounds);
+  const bounds = L.latLngBounds(points).pad(0.5);
+  map.fitBounds(bounds);
 
-map.scrollWheelZoom.disable();
+  map.scrollWheelZoom.disable();
+};
