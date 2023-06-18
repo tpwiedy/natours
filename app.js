dc08cded0fcc94713656885261b6efb391974462
tpwiedy <wiedy.pratama@gmail.com>
2023-06-18 14:55:21 +0700

    Implemented CORS

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
+const cors = require('cors');
 // const csp = require('express-csp');
 
 // Export Module
@@ -27,6 +28,10 @@ app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
 // 1) GLOBAL MIDDLEWARES
+// Implement CORS
+app.use(cors());
+app.options('*', cors());
+
 // Serving Static Files (to open /public/ file)
 app.use(express.static(path.join(__dirname, 'public')));
 
