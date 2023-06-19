0e2497b85685f60ae6dfbede42f378938e800a5d
tpwiedy <wiedy.pratama@gmail.com>
2023-06-19 15:51:58 +0700

    modified session url on stripe.js

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -6,8 +6,7 @@ export const bookTour = async (tourId) => {
   const stripe = Stripe('process.env.STRIPE_PUBLIC_KEY');
   try {
     // 1) Get checkout session from API
-    // const session = await axios(`api/v1/bookings/checkout-session/${tourId}`);
-    const session = await axios(`/checkout-session/${tourId}`);
+    const session = await axios(`/api/v1/bookings/checkout-session/${tourId}`);
     // console.log(session);
     // 2) Create checkout form + charge creadit card
     // await stripe.redirectToCheckout({
