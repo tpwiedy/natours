05b65e4bdc8d7305033498202ceff31d15c47150
tpwiedy <wiedy.pratama@gmail.com>
2023-06-19 15:46:59 +0700

    changed the url session on stipejs

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -57,6 +57,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 
 //   res.redirect(req.originalUrl.split('?')[0]);
 // });
+
 const createBookingCheckout = async (session) => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
--- public/js/stripe.js
+++ public/js/stripe.js
@@ -6,9 +6,8 @@ export const bookTour = async (tourId) => {
   const stripe = Stripe('process.env.STRIPE_PUBLIC_KEY');
   try {
     // 1) Get checkout session from API
-    const session = await axios(
-      `api/v1/bookings/checkout-session/${tourId}`
-    );
+    // const session = await axios(`api/v1/bookings/checkout-session/${tourId}`);
+    const session = await axios(`/checkout-session/${tourId}`);
     // console.log(session);
     // 2) Create checkout form + charge creadit card
     // await stripe.redirectToCheckout({
@@ -20,3 +19,5 @@ export const bookTour = async (tourId) => {
     showAlert('error', err);
   }
 };
+
+// https://natours-production-wiedy.up.railway.app/tour/api/v1/bookings/checkout-session/647565ac55acb29f7332de64
